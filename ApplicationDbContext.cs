efc1a672ff39d494c041519081af431a9bef03eb
Rishiprajapaati <166892285+Rishiprajapaati@users.noreply.github.com>
2025-10-30 21:58:24 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using StudentCRUD.Models;
+
+namespace StudentCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
